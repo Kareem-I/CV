20a3bc2c7789b18063348ca5522d91dba399f158
Kareem-I <kareemi@kth.se>
2021-06-03 22:08:19 +0200

    Updates

---
+++ precache-manifest.d144acd4adc40e7422d785e7c8530564.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614d4b6bdcb3932d0dcdb7171311909e",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "b8cfd44992777652f8f7",
+    "url": "/CV/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "03c9fb8613f2572e7550",
+    "url": "/CV/static/css/main.8ecf92e8.chunk.css"
+  },
+  {
+    "revision": "b8cfd44992777652f8f7",
+    "url": "/CV/static/js/2.c0f978d6.chunk.js"
+  },
+  {
+    "revision": "03c9fb8613f2572e7550",
+    "url": "/CV/static/js/main.3b1be8b8.chunk.js"
+  },
+  {
+    "revision": "21043815baf811eb5681",
+    "url": "/CV/static/js/runtime~main.50de0404.js"
+  },
+  {
+    "revision": "daa4ebfd71d421a21afadeed17986631",
+    "url": "/CV/static/media/Chalmers.daa4ebfd.png"
+  },
+  {
+    "revision": "17eee42ff182880497f367074822a54c",
+    "url": "/CV/static/media/KalleAnka.17eee42f.png"
+  },
+  {
+    "revision": "8c5ac0b29dad997bec4e6462a4668ed4",
+    "url": "/CV/static/media/kth.8c5ac0b2.png"
+  },
+  {
+    "revision": "df2624092843928870bcfbc86c8624f3",
+    "url": "/CV/static/media/vitrolife.df262409.png"
+  }
+]);
